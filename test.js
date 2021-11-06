864ffcff0a7b8d254eb9fe9a36d09b5a356e4ed5
raja <rdas828726@gmail.com>
2021-11-06 22:11:59 +0530

    java script

---
+++ test.js
@@ -0,0 +1,12 @@
+
+console.log("ss")//for printing a value
+const db=5
+console.log(db) 
+let a=4;
+console.log(a,"a before updation(let)")
+a=3;
+console.log(a," a after update(let) ")
+var v=13
+console.log(v," v before updation (var)")
+v=12
+console.log(v," v after updation(var)")
